15aa499da6df02f8f0dac2e80bbdd6b34510c7ba
gajanan patange <123582811+gajananhp13@users.noreply.github.com>
2025-01-21 13:14:57 +0530

    Create strongnumber.cpp
    strong number logic in c++

---
+++ basics/strongnumber.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+using namespace std;
+
+// function to calculate factorial
+int facto(int n){
+    int fact = 1;
+    
+    for(int i = 1; i <= n; i++)
+            fact = fact * i;
+    
+    return fact;
+}
+
+int detectStrong(int num){
+    
+    int digit, sum = 0;
+    int temp = num;
+    
+    // calculate 1! + 4! + 5!
+    while(temp!=0){
+        digit = temp % 10;
+        
+        sum = sum + facto(digit);
+        temp /= 10;
+    }
+    
+    // returns 1 if both equal else 0
+    return sum == num;
+    
+}
+int main ()
+{
+    int num = 145;
+    
+    if(detectStrong(num))
+        cout << num << " is Strong Number";
+    else
+        cout << num << " is Not Strong Number";
+
+}
+Output
+145 is Strong Number
+Related Pages
+Factor of a number
+
+Finding Prime Factors of a number
+
+Strong number
+
+Perfect number
+
+Perfect Square
+
+Automorphic number
+
+Method 2 (Recursive)
+Here we use recursion in c++ to find factorial of a number.
+
+C++ Code:-
+Run
+#include <iostream>
+using namespace std;
+
+// function to calculate factorial
+int facto(int num)
+{
+    if(num == 0)
+        return 1;
+        
+    return num * facto(num-1);
+}
+
+int detectStrong(int num){
+    
+    int digit, sum = 0;
+    int temp = num;
+    
+    // calculate 1! + 4! + 5!
+    while(temp!=0){
+        digit = temp % 10;
+        
+        sum = sum + facto(digit);
+        temp /= 10;
+    }
+    
+    // returns 1 if both equal else 0
+    return sum == num;
+    
+}
+int main ()
+{
+    int num = 145;
+    
+    if(detectStrong(num))
+        cout << num << " is Strong Number";
+    else
+        cout << num << " is Not Strong Number";
+
+}
