836b5e950c9aec56d35e976db35bfcbe499c5576
gajanan patange <123582811+gajananhp13@users.noreply.github.com>
2025-04-21 00:06:13 +0530

    Create friendly pair.cpp

---
+++ basics/friendly pair.cpp
@@ -0,0 +1,7 @@
+friendly pair 
+ALGORITHM:>
+We will write a code to Check Friendly pair in C++
+
+Two numbers num1 & num2 are friendly pairs if the following holds true -
+
+(Sum of divisors of num1)/num1= (Sum of divisors of num2)/num2
