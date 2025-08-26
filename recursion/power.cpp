aef4ef9bcc02757ec757da1c76731ae30d0ee682
gajananhp13 <gajananpatangepg@gmail.com>
2025-08-26 11:50:42 +0530

    Create power.cpp

---
+++ recursion/power.cpp
@@ -0,0 +1,18 @@
+    #include<iostream>
+    using namespace std;
+
+    int power(int n){
+
+        
+    }
+    int main()
+    {
+        int n;
+        cin>>n;
+        cout<<power(n);
+
+       return 0;
+    }  
+ 
+    
+
